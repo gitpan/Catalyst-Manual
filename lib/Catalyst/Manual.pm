2b1acd936f3da45dac7759aa67f9b5a182ec9bd2
Jonathan Rockway <jrockway@cpan.org>
2006-11-07 16:51:02 +0000

    Import of JROCKWAY/Catalyst-Manual-5.700401 from CPAN.
    gitpan-cpan-distribution: Catalyst-Manual
    gitpan-cpan-version:      5.700401
    gitpan-cpan-path:         JROCKWAY/Catalyst-Manual-5.700401.tar.gz
    gitpan-cpan-author:       JROCKWAY
    gitpan-cpan-maturity:     released

---
+++ lib/Catalyst/Manual.pm
@@ -0,0 +1,26 @@
+#!perl
+# Manual.pm 
+# Copyright (c) 2006 Jonathan Rockway <jrockway@cpan.org>
+
+package Catalyst::Manual;
+
+=head1 NAME
+
+Catalyst::Manual - pacakge holding the Catalyst developer's manual and tutorial
+
+=head1 SYNOPSIS
+
+    perldoc Catalyst::Manual::Intro
+    perldoc Catalyst::Manual::Tutorial
+
+=head1 SEE ALSO
+
+Install L<Task::Catalyst::Tutorial|Task::Catalyst::Tutorial> to
+install all the dependencies you need to follow along with the
+Tutorial.
+
+=cut
+
+our $VERSION = '5.700301';
+
+1;
