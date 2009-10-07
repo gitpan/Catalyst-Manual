23f516afd9a0aa06e96f381448545e4ad6b2b63f
Kennedy Clark <hkclark@cpan.org>
2009-10-07 09:14:09 +0000

    Import of HKCLARK/Catalyst-Manual-5.8001 from CPAN.
    gitpan-cpan-distribution: Catalyst-Manual
    gitpan-cpan-version:      5.8001
    gitpan-cpan-path:         HKCLARK/Catalyst-Manual-5.8001.tar.gz
    gitpan-cpan-author:       HKCLARK
    gitpan-cpan-maturity:     released

--- lib/Catalyst/Manual.pm
+++ lib/Catalyst/Manual.pm
@@ -6,16 +6,18 @@ package Catalyst::Manual;
 use strict;
 use warnings;
 
-our $VERSION = '5.8000';
+our $VERSION = '5.8001';
 
 =head1 NAME
 
 Catalyst::Manual - The Catalyst developer's manual
 
+
 =head1 SYNOPSIS
 
     perldoc Catalyst::Manual::Intro
     perldoc Catalyst::Manual::Tutorial
+
 
 =head1 SEE ALSO
 
@@ -35,37 +37,75 @@ L<Catalyst::Manual::About|Catalyst::Manual::About>
 
 =item *
 
-L<Catalyst::Manual::Intro|Catalyst::Manual::Intro>
+L<Catalyst::Manual::Intro>
 
 =item *
 
-L<Catalyst::Manual::Tutorial|Catalyst::Manual::Tutorial>
+L<Catalyst::Manual::Tutorial>
 
 =item *
 
-L<Catalyst::Manual::Actions|Catalyst::Manual::Actions>
+L<Catalyst::Manual::Actions>
 
 =item *
 
-L<Catalyst::Manual::Cookbook|Catalyst::Manual::Cookbook>
+L<Catalyst::Manual::Cookbook>
 
 =item *
 
-L<Catalyst::Manual::DevelopmentProcess|Catalyst::Manual::DevelopmentProcess>
+L<Catalyst::Manual::DevelopmentProcess>
 
 =item *
 
-L<Catalyst::Manual::ExtendingCatalyst|Catalyst::Manual::ExtendingCatalyst>
+L<Catalyst::Manual::ExtendingCatalyst>
 
 =item *
 
-L<Catalyst::Manual::Internals|Catalyst::Manual::Internals>
+L<Catalyst::Manual::Internals>
 
 =item *
 
 L<Catalyst::Manual::Plugins|Catalyst::Manual::Plugins>
 
 =back
+
+=head2 Books
+
+For additional information on Catalyst, there are currently two books available:
+
+=over 4
+
+=item *
+
+The Definitive Guide to Catalyst: Writing Extendable, Scalable and 
+Maintainable Perl–Based Web Applications
+
+By: Kieren Diment, Matt Trout 
+Available July 12, 2009
+ISBN 10: 1-4302-2365-0
+ISBN 13: 978-1-4302-2365-8
+http://www.apress.com/book/view/9781430223658
+
+=item *
+
+Accelerating Perl Web Application Development
+
+By: Jonathan Rockway
+Published December, 2007
+ISBN 10: 1847190952
+ISBN 13: 978-1-847190-95-6
+http://www.packtpub.com/catalyst-perl-web-application/book
+
+=back
+
+=head1 AUTHORS
+
+Catalyst Contributors, see Catalyst.pm
+
+=head1 COPYRIGHT
+
+This library is free software. You can redistribute it and/or modify it under
+the same terms as Perl itself.
 
 =cut
 
